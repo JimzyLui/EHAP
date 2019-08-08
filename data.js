812e9c443c502cd6874a9cdef378ec89b4b0b1a9
Jimzy Lui <Jimzy.Lui@gmail.com>
2019-08-07 21:23:50 -1000

    initial data to populate dropdowns

---
+++ data.js
@@ -0,0 +1,7 @@
+const arrFloors = [];
+const arrRooms = [];
+const arrTargetAreas = ['wall','closet','bookcase','cabinet','corner'];
+const arrQualifiers = ['right','left','above','below','between','top','bottom','outside','inside'];
+const arrBrands = ['Target','Best Buy'];
+const arrStores = ['Target','Safeway','Longs'];
+const arrCountFromBottom = Array.from(Array(15).keys());
